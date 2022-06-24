b84838192c065b76a905779808d18fe8b39f167b
Vic <104384086+ProfesorV@users.noreply.github.com>
2022-06-24 10:58:35 -0400

    6/24/22

--- Hacker Rank Solutions Better Explained/RecursionFibonacciNumbers.cs
+++ Hacker Rank Solutions Better Explained/RecursionFibonacciNumbers.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.IO;
 
 class Solution {
-    
+    //int
     public static int Fibonacci(int n) {
+        //if condition(int < 2) return
         if(n < 2) return n;
+        //return function (int - 2) + function (int -1)
         return Fibonacci(n - 2) + Fibonacci(n - 1);
     }
-
     static void Main(String[] args) {
         int n = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine(Fibonacci(n));
